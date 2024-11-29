89b5ddb3f3a120fc00e0df18e961fb45df733e90
Matheus Repizo <matheusdprepizo@gmail.com>
2024-11-29 14:37:31 -0300

    Feat: Implementando o TODO do Método calcularValorDiaria.

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -1,6 +1,3 @@
-using System.ComponentModel;
-using System.Linq.Expressions;
-
 namespace DesafioProjetoHospedagem.Models
 {
     public class Reserva
@@ -20,22 +17,22 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
 
-            try{ // Inicializa e delimita um bloco de código, onde uma exceção possa ocorrer.
-                if (hospedes.Count() <= Suite.Capacidade) // Gera uma condição, verificando se a capacidade da Suite comporta a quantidade de hospedes cadastrados.
+            try // Inicializa e delimita um bloco de código, onde uma exceção possa ocorrer.
+            { 
+                if (hospedes.Count <= Suite.Capacidade) // Gera uma condição, verificando se a capacidade da Suite comporta a quantidade de hospedes cadastrados.
                 {
-                    Hospedes = hospedes; // Atribui a propriedade Hospedes, a lista hospedes que o método iŕa receber.
-                     // Se verdadeiro, adiciona os hospedes da lista recebida do método à lista Hospedes do Objeto.
+                    Hospedes = hospedes; // Se verdadeiro, adiciona os hospedes da lista recebida do método à lista Hospedes do Objeto.
                 }
                 else
                 {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido.
-                     throw new ArgumentException("A suite não comporta a quantidade de hospedes cadastrada!"); // Se falso, lança uma exceção,interrompendo o fluxo do código.
+                    // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido.
+                    throw new ArgumentException("A suite não comporta a quantidade de hospedes cadastrada!"); // Se falso, lança uma exceção,interrompendo o fluxo do código.
                 }
-                }
-                catch (ArgumentException ex) // Trata a exceção gerada.
-                {
-                    Console.WriteLine(ex.Message); // Mostra no console apenas a mensagem informada na exceção.
-                }    
+            }
+            catch (ArgumentException ex) // Trata a exceção gerada.
+            {
+                Console.WriteLine(ex.Message); // Mostra no console apenas a mensagem informada na exceção.
+            }
         }
 
         public void CadastrarSuite(Suite suite)
@@ -46,12 +43,12 @@ namespace DesafioProjetoHospedagem.Models
         public int ObterQuantidadeHospedes()
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            if (Hospedes == null) {
-                return 0;
+            if (Hospedes == null) // Valida se a Lista Hospedes está vazia.
+            {
+                return 0; // Se verdadeiro retorna 0;
             }
-            
-            return Hospedes.Count();
+
+            return Hospedes.Count; // Caso contrário, retorna o total de hospedes cadastrados.
 
         }
 
@@ -59,17 +56,25 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            decimal totalSemDesconto = DiasReservados * Suite.ValorDiaria; // Atribui a Variavel do tipo decimal totalSemDesconto o valor da multiplicação dos DiasReservados x valorDiaria.
+            decimal DescontoPorcentagem = 10.00M; // Atribui a Variavel do tipo decimal DescontoPorcentagem o valor do percentual que será aplicado o desconto.
+            decimal valorComDesconto = totalSemDesconto * (1 - DescontoPorcentagem /100); // Atribui a Variavel do tipo decimal valorComDescont o Valor com o Desconto.
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+            
+            // Verifica se a Lista é vazia
+            if( Hospedes == null) {
+                return 0; // Retorna o valor zero.
+            }
+             // Verifica se os dias reservados é menor que 10 dias.
+            if (DiasReservados < 10)
             {
-                valor = 0;
+                return totalSemDesconto; // Retorna o valor sem desconto.
+            }
+            else {
+                return valorComDesconto ; // Caso o contrário retorna o valor aplicado o desconto.
             }
 
-            return valor;
         }
     }
 }
